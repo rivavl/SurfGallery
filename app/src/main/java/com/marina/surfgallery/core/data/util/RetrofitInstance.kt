cdbd5b78d2fa2940145971b36e0c3699ba4ab898
rivavl <mvkalinenkova@yandex.ru>
2022-07-14 22:36:45 +0300

    Add RetrofitInstance

---
+++ app/src/main/java/com/marina/surfgallery/core/data/util/RetrofitInstance.kt
@@ -0,0 +1,33 @@
+package com.marina.surfgallery.core.data.util
+
+import com.marina.surfgallery.auth.data.remote.AuthApi
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class RetrofitInstance {
+    companion object {
+
+        private const val BASE_URL = "https://pictures.chronicker.fun/api/"
+
+        private val retrofit by lazy {
+            val logging = HttpLoggingInterceptor()
+            logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+            val client = OkHttpClient.Builder()
+                .addInterceptor(logging)
+                .build()
+
+            Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build()
+        }
+
+        val authApi by lazy {
+            retrofit.create(AuthApi::class.java)
+        }
+
+    }
+}
