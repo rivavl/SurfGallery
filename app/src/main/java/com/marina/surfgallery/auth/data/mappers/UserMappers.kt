51a0d838217e2f24fa31887b4ee80c11ef076f78
rivavl <mvkalinenkova@yandex.ru>
2022-07-14 22:38:46 +0300

    Add mappers in data layer

---
+++ app/src/main/java/com/marina/surfgallery/auth/data/mappers/UserMappers.kt
@@ -0,0 +1,21 @@
+package com.marina.surfgallery.auth.data.mappers
+
+import com.marina.surfgallery.auth.data.remote.entity.response.UserDto
+import com.marina.surfgallery.core.domain.entity.UserInfo
+
+/*
+* Маппер data -> domain
+* */
+fun UserDto.toUserInfo(token: String): UserInfo {
+    return UserInfo(
+        id = id,
+        firstName = firstName,
+        lastName = lastName,
+        quote = about,
+        photo = avatar,
+        city = city,
+        email = email,
+        phone = phone,
+        token = token
+    )
+}
