e10062b95319bda8c9345a9cffaf3841fcd96f5f
rivavl <mvkalinenkova@yandex.ru>
2022-07-15 21:47:10 +0300

    Add ui for home fragment

---
+++ app/src/main/java/com/marina/surfgallery/home/presentation/HomeFragmentViewModel.kt
@@ -0,0 +1,48 @@
+package com.marina.surfgallery.home.presentation
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.marina.surfgallery.auth.domain.util.Resource
+import com.marina.surfgallery.home.domain.use_case.GetAllPicturesUseCase
+import com.marina.surfgallery.home.presentation.entity.PictureItem
+import com.marina.surfgallery.home.presentation.mapper.toPresentation
+import kotlinx.coroutines.launch
+
+class HomeFragmentViewModel(
+    private val getAllPicturesUseCase: GetAllPicturesUseCase
+) : ViewModel() {
+
+    private var _picturesList = MutableLiveData<Resource<List<PictureItem>>>()
+
+    val picturesList: LiveData<Resource<List<PictureItem>>> get() = _picturesList
+
+    init {
+        getPictures()
+    }
+
+    private fun getPictures() = viewModelScope.launch {
+        getAllPicturesUseCase().collect { result ->
+            println("000000000000000000000000000000000000")
+            println(result.data)
+            when (result) {
+                is Resource.Success -> {
+                    _picturesList.postValue(
+                        Resource.Success(
+                            result.data?.toPresentation() ?: listOf()
+                        )
+                    )
+                }
+
+                is Resource.Loading -> {
+                    _picturesList.postValue(Resource.Loading())
+                }
+
+                is Resource.Error -> {
+                    _picturesList.postValue(Resource.Error(_picturesList.value?.message.toString()))
+                }
+            }
+        }
+    }
+}
