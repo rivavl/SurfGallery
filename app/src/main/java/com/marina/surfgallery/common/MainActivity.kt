749495841575e2dab932151c27fa2e1d2ed3016c
rivavl <mvkalinenkova@yandex.ru>
2022-07-22 23:34:52 +0300

    Add splash screen

--- app/src/main/java/com/marina/surfgallery/common/MainActivity.kt
+++ app/src/main/java/com/marina/surfgallery/common/MainActivity.kt
@@ -1,16 +1,56 @@
 package com.marina.surfgallery.common
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.marina.surfgallery.auth.presentation.LoginFragment
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.marina.surfgallery.R
+import com.marina.surfgallery.core.presentation.fragment.FavoriteFragment
+import com.marina.surfgallery.core.presentation.fragment.HomeFragment
+import com.marina.surfgallery.profile.presentation.ProfileFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                viewModel.isLoading.value == true
+            }
+        }
         setContentView(R.layout.activity_main)
+
+//        launchFragment(LoginFragment())
+        findViewById<BottomNavigationView>(R.id.bottom_nav_bar).setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.menu_main -> {
+                    launchFragment(HomeFragment())
+                    true
+                }
+
+                R.id.menu_favorite -> {
+                    launchFragment(FavoriteFragment())
+                    true
+                }
+
+                R.id.menu_profile -> {
+                    launchFragment(ProfileFragment())
+                    true
+                }
+                else -> {
+                    false
+                }
+            }
+        }
+    }
+
+    private fun launchFragment(fragment: Fragment) {
         supportFragmentManager.beginTransaction()
-            .replace(R.id.container, LoginFragment())
+            .replace(R.id.container, fragment)
             .commit()
     }
 }
---
+++ app/src/main/java/com/marina/surfgallery/common/MainViewModel.kt
@@ -0,0 +1,21 @@
+package com.marina.surfgallery.common
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class MainViewModel : ViewModel() {
+
+    private val _isLoading = MutableLiveData(true)
+    val isLoading: LiveData<Boolean> get() = _isLoading
+
+    init {
+        viewModelScope.launch {
+            delay(500)
+            _isLoading.value = false
+        }
+    }
+}
